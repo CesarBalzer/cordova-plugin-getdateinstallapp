6a16b5e216e23ced420af48d33dbdcca9d7202bd
CesarBalzer <cesarebalzer@gmail.com>
2016-12-12 20:56:39 -0200

    Commit inicial

--- src/android/Getdateinstallapp.java
+++ src/android/Getdateinstallapp.java
@@ -3,6 +3,24 @@ package br.com.balzer.getdateinstallapp.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import android.annotation.SdkConstant;
+import android.annotation.SdkConstant.SdkConstantType;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.pm.ManifestDigest;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.AndroidException;
+import android.util.DisplayMetrics;
 
 public class Getdateinstallapp extends CordovaPlugin {
 
@@ -10,17 +28,28 @@ public class Getdateinstallapp extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("getdate")) {
-
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
-
             return true;
-
+        } else if (action.equals("getteste")) {
+            String name = data.getString(0);
+            String message = "Ola , " + name;
+            callbackContext.success(message);
+            return true;
         } else {
-            
             return false;
-
         }
+    }
+
+    void getInstallDate(CallbackContext callbackContext) {
+        String errMsg = null;
+        PackageManager pm = context.getPackageManager();
+        ApplicationInfo appInfo = pm.getApplicationInfo("br.com.balzer.getdateinstallapp", 0);
+        String appFile = appInfo.sourceDir;
+        long installed = new File(appFile).lastModified();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String installTime = dateFormat.format(new Date(packageInfo.firstInstallTime));
+        callbackContext.success(installTime);
     }
 }
