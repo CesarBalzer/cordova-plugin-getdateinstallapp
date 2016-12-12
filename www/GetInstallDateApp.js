8172909866441ebdccd8304570fbd788989394a7
CesarBalzer <cesarebalzer@gmail.com>
2016-12-12 17:20:57 -0200

    Commit inicial

--- www/GetInstallDateApp.js
+++ www/GetInstallDateApp.js
@@ -1,10 +1,11 @@
 var exec = require('cordova/exec');
+
 var DateAPP = {
     fodase: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "GetInstallDateApp", "greet", [name]);
+        exec(successCallback, errorCallback, "GetInstallDateApp", "greet", [name]);
     },
     fodasedenovo: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "GetInstallDateApp", "mame", [name]);
+        exec(successCallback, errorCallback, "GetInstallDateApp", "mame", [name]);
     }
 };
 module.exports = DateAPP;
