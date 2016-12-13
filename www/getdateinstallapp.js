4282f2e9e995c05245aefe2c7fce09949a36cd55
CesarBalzer <cesarebalzer@gmail.com>
2016-12-12 22:53:04 -0200

    Commit inicial

--- www/getdateinstallapp.js
+++ www/getdateinstallapp.js
@@ -3,6 +3,9 @@ var exec = require('cordova/exec');
 var Gdia = {
     getdatetime: function (name, successCallback, errorCallback) {
         exec(successCallback, errorCallback, "Getdateinstallapp", "getdatetime", [name]);
+    },
+    getupdatetime: function (name, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "Getdateinstallapp", "getupdatetime", [name]);
     }
 };
 module.exports = Gdia;
