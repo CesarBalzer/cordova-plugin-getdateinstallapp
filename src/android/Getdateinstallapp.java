b4cb386e0028bcbc73aef9efaa1f40d4d96fd795
CesarBalzer <cesarebalzer@gmail.com>
2016-12-12 22:37:30 -0200

    Commit inicial

--- src/android/Getdateinstallapp.java
+++ src/android/Getdateinstallapp.java
@@ -18,59 +18,33 @@ public class Getdateinstallapp extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
     	Context context=this.cordova.getActivity().getApplicationContext();
-        if (action.equals("getdate")) {
+        if (action.equals("getdatetime")) {
             String name = data.getString(0);
-            getInstallDate(callbackContext,name,context);
+            getInstallDateTime(callbackContext,name,context);
             return true;
-        } else if (action.equals("getteste")) {
-            String name = data.getString(0);
-            String message = "Ola , " + name;
-            callbackContext.success(message);
+        } else if (action.equals("getupdatetime")) {
+        	String name = data.getString(0);
+            getInstallUpdateTime(callbackContext,name,context);
             return true;
         } else {
             return false;
         }
     }
 
-    void getInstallDate(CallbackContext callbackContext,String name, Context context) {
-    	
+    void getInstallDateTime(CallbackContext callbackContext,String packageName, Context context) {
 		String installTime = null;
-		Date updateTime = null;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		//context context = null;
-		
 		PackageManager pm = context.getPackageManager();
 		ApplicationInfo appInfo = null;
-		String packageName = "com.example.appplugin";
-		
 		try {
-			appInfo = pm.getApplicationInfo("com.example.appplugin", 0);
-			System.out.println("--------------"+appInfo);
+			appInfo = pm.getApplicationInfo(packageName, 0);
 			PackageInfo packageInfo = pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
 			installTime = dateFormat.format( new Date( packageInfo.firstInstallTime ) );
-			updateTime = new Date( packageInfo.lastUpdateTime );
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String appFile = appInfo.sourceDir;
-		long installed = new File(appFile).lastModified();
-		
-		System.out.println("DATA DE INSTALACAO-------"+installed);
-		
-		System.out.println("DATA DE INSTALACAO-------"+installTime);
-		System.out.println("DATA DE ATUALIZACAO-------"+updateTime);
 		callbackContext.success(installTime);
     	
-//        String errMsg = null;
-//        try {
-//            String message = "Hello, " + name;
-//            callbackContext.success(message);
-//            
-//        } catch (Exception e) {
-//            errMsg = e.getMessage();
-//            e.printStackTrace();
-//            callbackContext.error(errMsg);
-//        }
     }
 }
